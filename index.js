7060429ebd2f6b5d3f00293431e9f885cfb0b663
Sauren Sharma <sharmasaurenb@gmail.com>
2024-06-25 15:41:31 +0530

    First Commit

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import client from "./db/dbconnect.js";
 import Shop from "./models/shops.model.js";
 import { User } from "./models/user.model.js";
 import findUserById from "./middleware/findUser.js";
-
 const PORT = 8080;
 const app = express();
 
@@ -73,7 +72,7 @@ app.get("/api/users/:userId/shops", async (req, res) => {
     const users = await collection.find().toArray();
 
     const current_user = users.filter((user) => user.clerkId === userId);
-    console.log(current_user[0].shops);
+
     res.json(current_user[0].shops);
   } catch (err) {
     console.log(err);
@@ -81,62 +80,108 @@ app.get("/api/users/:userId/shops", async (req, res) => {
   }
 });
 
-app.post(
-  "/api/users/:userId/shops/:id/items",
-  findUserById,
-  async (req, res) => {
-    const { id } = req.params;
-    const { itemName, quantity } = req.body;
+app.get("/api/users/:userId/shops/:shopId", async (req, res) => {
+  const userId = req.params.userId;
+  const shopId = req.params.shopId;
 
-    const shop = await Shop.findById(id);
-    if (shop) {
-      const newItem = {
-        id: Math.random().toString(),
-        itemName: itemName,
-        quantity: quantity,
-      };
-      shop.items.push(newItem);
-      shop.items.save();
-      res.status(201).send(newItem);
-    } else {
-      res.status(404).send({ message: "Shop not found " });
-    }
-  }
-);
+  try {
+    const database = client.db("users");
+    const collection = database.collection("users");
+    const users = await collection.find().toArray();
+    const current_user = users.filter((user) => user.clerkId === userId);
+    const shopIndex = current_user[0].shops.findIndex(
+      (shop) => shop.id === shopId
+    );
 
-app.delete(
-  "/api/user/:userId/shops/:shopId/items/:itemId",
-  findUserById,
-  async (req, res) => {
-    const { shopId, itemId } = req.params;
-
-    const shop = await Shop.findById(shopId);
-    if (shop) {
-      const itemIndex = shop.items.id(itemId);
-      if (itemIndex) {
-        itemIndex.remove();
-        await shop.save();
-        res.status(200).send({ message: "Item deleted successfully" });
-      } else {
-        res.status(404).send({ message: "Item not found" });
-      }
-    } else {
-      res.status(404).send({ message: "Shop not found" });
-    }
-  }
-);
-
-app.delete("/api/user/:userId/shops/:id", findUserById, async (req, res) => {
-  const { id } = req.params;
-  const shop = await Shop.findByIdAndDelete(id);
-  if (shop) {
-    res.status(200).send({ message: "Shop deleted" });
-  } else {
-    res.status(404).send({ message: "Shop not found" });
+    res.status(200).send(current_user[0].shops[shopIndex]);
+  } catch (err) {
+    res.status(500).send({ message: "Could not update shop" });
   }
 });
 
-app.post("/api/user/:userId/shops", async (req, res) => {
+app.post("/api/users/:userId/shops/:shopId/items", async (req, res) => {
+  const { userId, shopId } = req.params;
+  const { itemName, quantity } = req.body;
+  try {
+    const database = client.db("users");
+    const collection = database.collection("users");
+    const users = await collection.find().toArray();
+
+    const current_user = users.filter((user) => user.clerkId === userId);
+    const shopIndex = current_user[0].shops.findIndex(
+      (shop) => shop.id === shopId
+    );
+    const shop = current_user[0].shops[shopIndex];
+    console.log(shop);
+
+    const newItem = {
+      id: Math.random().toString(),
+      itemName: itemName,
+      quantity: quantity,
+    };
+    shop.items.push(newItem);
+    await collection.updateOne(
+      { clerkId: userId },
+      { $set: { shops: current_user[0].shops } }
+    );
+    res.status(201).send(newItem);
+  } catch (err) {
+    res.status(500).send({ message: "Could not Add Item" });
+  }
+});
+
+app.delete(
+  "/api/users/:userId/shops/:shopId/items/:itemId",
+  async (req, res) => {
+    const { userId, shopId, itemId } = req.params;
+
+    try {
+      const database = client.db("users");
+      const collection = database.collection("users");
+      const users = await collection.find().toArray();
+
+      const current_user = users.filter((user) => user.clerkId === userId);
+      const shopIndex = current_user[0].shops.findIndex(
+        (shop) => shop.id === shopId
+      );
+      const shop = current_user[0].shops[shopIndex];
+      current_user[0].shops[shopIndex].items = shop.items.filter(
+        (item) => item.id !== itemId
+      );
+      await collection.updateOne(
+        { clerkId: userId },
+        { $set: { shops: current_user[0].shops } }
+      );
+      res.status(200).send({ message: "Item Deleted Sucessfully" });
+    } catch (err) {
+      res.status(500).send({ message: "Could not delete Item" });
+    }
+  }
+);
+
+app.delete("/api/users/:userId/shops/:shopId", async (req, res) => {
+  const { userId, shopId } = req.params;
+  try {
+    const database = client.db("users");
+    const collection = database.collection("users");
+    const users = await collection.find().toArray();
+    const current_user = users.filter((user) => user.clerkId === userId);
+
+    const updatedShops = current_user[0].shops.filter(
+      (shop) => shop.id !== shopId
+    );
+    await collection.updateOne(
+      { clerkId: userId },
+      { $set: { shops: updatedShops } }
+    );
+    res.json({ message: "Done successfully" });
+  } catch (err) {
+    console.error("Error deleting shop: ", err);
+    res.status(500).send({ message: "Error deleting shop" });
+  }
+});
+
+app.post("/api/users/:userId/shops", async (req, res) => {
   const { userId } = req.params;
   const { title } = req.body;
   const newShop = {
@@ -147,7 +192,7 @@ app.post("/api/user/:userId/shops", async (req, res) => {
 
   try {
     const database = client.db("users");
-    const collection = database.collection("user");
+    const collection = database.collection("users");
     const users = await collection.find().toArray();
     const current_user = users.filter((user) => user.clerkId === userId);
     if (!current_user) {
@@ -159,23 +204,37 @@ app.post("/api/user/:userId/shops", async (req, res) => {
       { clerkId: userId },
       { $set: { shops: current_user[0].shops } }
     );
-    res.status(201).send(newShop);
+    res.status(200).send(current_user[0].shops);
   } catch (err) {
     res.status(500).send({ message: "Error adding Shop" });
   }
 });
 
-app.put("/api/user/:userId/shops/:shopId", findUserById, async (req, res) => {
-  const { shopId } = req.params.shopId;
+app.put("/api/users/:userId/shops/:shopId", async (req, res) => {
+  const { userId, shopId } = req.params;
   const { newTitle } = req.body;
 
-  const shop = req.user.shops.id(shopId);
-  if (shop) {
-    shop.title = newTitle;
-    await req.user.save();
-    res.status(200).send(shop);
-  } else {
-    res.status(404).send({ message: "Shop not found" });
+  try {
+    const database = client.db("users");
+    const collection = database.collection("users");
+    const users = await collection.find().toArray();
+    const current_user = users.filter((user) => user.clerkId === userId);
+
+    const shopIndex = current_user[0].shops.findIndex(
+      (shop) => shop.id === shopId
+    );
+    current_user[0].shops[shopIndex].title = newTitle;
+    await collection.updateOne(
+      { clerkId: userId },
+      {
+        $set: { shops: current_user[0].shops },
+      }
+    );
+
+    res.json(current_user[0].shops[shopIndex]);
+  } catch (err) {
+    console.error("Error updating Shop: ", err);
+    res.status(500).send({ message: "Could not update shop" });
   }
 });
 
